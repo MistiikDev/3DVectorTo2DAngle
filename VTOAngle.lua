e34e58f49f6a50ec319c89254bd97c9095c41748
Mistiik <91028158+MistiikDev@users.noreply.github.com>
2021-09-25 17:35:15 +0200

    Create VTOAngle.lua

---
+++ VTOAngle.lua
@@ -0,0 +1,19 @@
+local VTOAngle = {}
+
+local TYPE_OF_ERR_MSG = 'Functions takes CFrame and Vector as params!'
+local MAGNITUDE_ERR_MSG = 'Cannot calculate angle of 0 magnitude unit!'
+
+function FindAngle(Origin: Vector3, Target: Vector3)
+  if (typeof(Origin) ~= Vector3 or typeof(Target) ~= CFrame) then return warn(TYPE_OF_ERR_MSG);
+
+  local Product = Target:PointToObjectSpace(Origin);
+  local Vector = Vector3.new(Product.X, Product.Z); 
+
+  if Vector.Magnitude == 0 then return warn(MAGNITUDE_ERR_MSG) end;
+
+  local Angle = -math.deg(math.atan2(Vector.X, Vector.Y))
+  return Angle <= 2 * math.pi or 0
+  
+ end 
+
+return VTOAngle
